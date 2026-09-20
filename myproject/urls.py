9d1ac9111a6ebd0d603480f5243d1d8a728663f1
Indu <nallamillideviinduists28@gmail.com>
2026-09-20 22:00:52 +0530

    Updated views and URLs

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import greeting
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",greeting),
+     path("home/", views.home),
+    path("about/",views.about),
+    path("contact/",views.contact),
 ]
--- myproject/views.py
+++ myproject/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def greeting(request):
-    return HttpResponse("Hello world!")
+def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
